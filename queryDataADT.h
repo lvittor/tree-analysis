146db0641066b4ee8a83ed2ffdc7da72335614fa
mbartellini <mbartellini@itba.edu.ar>
2020-07-08 17:55:15 -0300

    Inicio de comentado de queryDataADT.h

--- queryDataADT.h
+++ queryDataADT.h
@@ -3,35 +3,37 @@
 
 typedef struct queryDataCDT * queryDataADT;
 
-/* Función: 
-*  Uso:    
-*  Descripción:
+/* Función: Crea una nueva instancia de queryDataADT
+*  Uso: qd = newQueryData(void);
+*  Descripción: -
 */
 queryDataADT newQueryData(void);
 
-/* Función: 
-*  Uso:
-*  Descripción:
+/* Función: Almacena la información del barrio
+*  Uso: addNbh(qd, name, population);
+*  Descripción: Agrega un barrio de nombre "name" y su cantidad de habitantes "population".
+*  Si ya se había agregado el mismo nombre, no hace nada.
 */
 void addNbh(queryDataADT qd, char * name, size_t population);
 
-/* Función:
-*  Uso:
-*  Descripción:
+/* Función: Almacena la información del arbol
+*  Uso: addTree(queryDataADT qd, const char * nbhName);
+*  Descripción: Agrega un árbol cuyo barrio es "nbhName".
+*  De no existir el barrio, no hace nada.
 */
 void addTree(queryDataADT qd, const char * nbhName);
 
-/* Función:
-*  Uso:
-*  Descripción:
+/* Función: Libera la memoria almacenada para el TAD
+*  Uso: freeQueryData(qd);
+*  Descripción: -
 */
 void freeQueryData(queryDataADT qd);
 
-/* Función:
-*  Uso:
+/* Función: Prepara los datos según el criterio de la query 1
+*  Uso: beginQuery1(qd);
 *  Descripción:
 */
-void toBegin(queryDataADT qd);
+void beginQuery1(queryDataADT qd);
 
 /* Función:
 *  Uso:
@@ -44,11 +46,5 @@ int hasNext(queryDataADT qd);
 *  Descripción:
 */
 char ** answer(queryDataADT qd, size_t * size);
-
-/* Función:
-*  Uso:
-*  Descripción:
-*/
-void beginQuery1(queryDataADT qd);
 
 #endif
