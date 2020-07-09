aa08469b99625bdd5fabc6a2c2cefd33d7f18b83
mbartellini <mbartellini@itba.edu.ar>
2020-07-09 01:01:42 -0300

    Agregado de chequeo para fallos de malloc

--- queryDataADT.c
+++ queryDataADT.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include "queryDataADT.h"
+
+#define MAX_QUERY 3
+
+#define fabs(n) ((n) < 0 ? -(n) : (n))
 
 static char * strDuplicate(const char * src) {
     char * dst = malloc(strlen(src) + 1);   // Guarda espacio para el nuevo string.
-    if (dst != NULL)                        // Si hay memoria,
-      strcpy(dst, src);                     // hace la copia
+    if(dst == NULL)                         // Si no hay memoria
+        perror("Error en malloc()");        // error.
+    else                                    // Si no,
+        strcpy(dst, src);                   // hace la copia
     return dst;                             // Retorna el nuevo string
 }
 
 static char * allocStringFromSize(size_t n) {
-  size_t len = snprintf (NULL, 0, "%lu", n);       // Devuelve la cantidad de caracteres del numero
-  char * ans = malloc((len + 1) * sizeof(ans[0])); // Guarda espacio para el nuevo string
-  if (ans != NULL)                                 // Si hay memoria,
-    snprintf(ans, len + 1, "%lu", n);              // Guarda como string el valor de n
-  return ans;                                      // Retorna el nuevo string
+    size_t len = snprintf (NULL, 0, "%lu", n);       // Devuelve la cantidad de caracteres del numero
+    char * ans = malloc((len + 1) * sizeof(ans[0])); // Guarda espacio para el nuevo string
+    if(ans == NULL)                                  // Si no hay memoria,
+        perror("Error en malloc()");                 // error.
+    else                                             // Si no,
+        snprintf(ans, len + 1, "%lu", n);            // Guarda como string el valor de n
+    return ans;                                      // Retorna el nuevo string
+}
+
+static char * allocStringFromFloat(float f) {
+    f = (int)(f * 100) / 100.0;                         // Trunca a dos decimales
+    size_t len = snprintf (NULL, 0, "%.2f", f);       // Devuelve la cantidad de caracteres del numero ya truncado
+    char * ans = malloc((len + 1) * sizeof(ans[0]));  // Guarda espacio para el nuevo string
+    if(ans == NULL)                                   // Si no hay memoria,
+        perror("Error en malloc()");                  // error.
+    else                                              // Si no,
+        snprintf(ans, len + 1, "%.2f", f);            // Guarda como string el valor de f
+    return ans;                                       // Retorna el nuevo string
 }
 
 /* Estructura que guarda la información necesaria para Q1 y Q2
@@ -55,6 +75,7 @@ typedef struct queryDataCDT {
 } queryDataCDT;
 
 /* --- Queries --- */
+typedef int (*pCompFunc)(nbhInfo *, nbhInfo *);
 // Query 1
 // ->{NOMBRE_BARRIO, CANT_DE_ARBOLES}
 // Se debe ordenar de manera decreciente por la cantidad de arboles.
@@ -74,19 +95,65 @@ char ** infoQ1(nbhInfo nbh) {
   return ans;
 }
 
+// Query 1
+// ->{NOMBRE_BARRIO, CANT_DE_ARBOLES/CANT_DE_HABITANTES}
+// Se debe ordenar de manera decreciente por la cantidad de arboles.
+// Si son iguales, se prioriza el orden alfabetico de los barrios.
+int compareQ2(nbhInfo * info1, nbhInfo * info2){
+  float ratio1 = (float)info1->trees/info1->population;
+  float ratio2 = (float)info2->trees/info2->population;
+  ratio1 = (int)(ratio1 * 100)/100.0;
+  ratio2 = (int)(ratio2 * 100)/100.0;
+  float c = ratio2 - ratio1;
+
+  if(fabs(c) < EPSILON)
+    return strcmp(info1->name, info2->name);
+  else
+    return (c > 0) ? 1 : -1;
+}
+
+// ->{NOMBRE_BARRIO, CANT_DE_ARBOLES/CANT_DE_HABITANTES}
+char ** infoQ2(nbhInfo nbh) {
+  char ** ans = malloc( 2 * sizeof(ans[0]) );
+  ans[0] = strDuplicate(nbh.name);
+  #if DEBUG
+    printf("Valor original: %f\n", (float)nbh.trees/nbh.population);
+  #endif
+  ans[1] = allocStringFromFloat((float)nbh.trees/nbh.population);
+  return ans;
+}
+
+//Proximamente.. ;)
+int compareQ3(nbhInfo * info1, nbhInfo * info2){
+  return 0;
+}
+
+char ** infoQ3(void){
+  return NULL;
+}
+
+
 queryDataADT newQueryData(void){
-  return calloc(1, sizeof(queryDataCDT)); //validar NULL
+  queryDataADT qd;
+  if( (qd = calloc(1, sizeof(queryDataCDT))) == NULL )
+    perror("Error en calloc()");
+  return qd;
 }
 
 static nbhList addNbhRec(nbhList list, nbhInfo * newNbh, int * ok, int (*criteria) (nbhInfo *, nbhInfo *)) {
   int c;
   if (list == NULL || (c = criteria(newNbh, &list->info)) < 0) {
-    nbhNode * newNode = malloc(sizeof(nbhNode)); // validar NULL
+    nbhNode * newNode;
+    if((newNode = malloc(sizeof(nbhNode))) == NULL ){
+      perror("Error en malloc()");
+      *ok = ERROR;
+      return list;
+    }
     newNode->info.trees = newNbh->trees;
     newNode->info.population = newNbh->population;
     newNode->info.name = strDuplicate(newNbh->name);
     newNode->tail = list;
-    *ok = 1;
+    *ok = SUCCESS;
     return newNode;
   }
   if( c > 0 )
@@ -100,7 +167,7 @@ static int compareByName(nbhInfo * info1, nbhInfo * info2){
 }
 
 void addNbh(queryDataADT qd, char * name, size_t population){
-  int ok = 0;
+  int ok = !SUCCESS;
   nbhInfo newNbh = { name, 0, population };
   qd->firstNbh = addNbhRec(qd->firstNbh, &newNbh, &ok, compareByName);
   if(ok){
@@ -113,13 +180,12 @@ void addNbh(queryDataADT qd, char * name, size_t population){
 *  donde se podrá hacer binSearch para comparar el string e
 *  incrementar la cantidad de árboles.
 */
-static void listToArray(queryDataADT qd){
+static int listToArray(queryDataADT qd){
   nbhInfo * tmp;
   qd->arrDim = qd->listDim;
-  tmp = realloc(qd->nbhArr, qd->arrDim * sizeof(qd->nbhArr[0]));
-  if(tmp == NULL){
-      fprintf(stderr, "No memory\n");                             // utilizar errno
-      exit(1);                                                    // no abortar en backend
+  if((tmp = realloc(qd->nbhArr, qd->arrDim * sizeof(qd->nbhArr[0]))) == NULL){
+      perror("Error en realloc()");
+      return ERROR;
   }
   qd->nbhArr = tmp;
   nbhList aux = qd->firstNbh;
@@ -133,6 +199,7 @@ static void listToArray(queryDataADT qd){
 
     aux = aux->tail;
   }
+  return SUCCESS;
 }
 
 /* Retorna el indice donde se encontraba nbhName en el arreglo arr.
@@ -154,29 +221,17 @@ static int binSearch(const char * nbhName, nbhInfo * arr, size_t size) {
 }
 
 // Encuentra el barrio en el vector creado por listToArray e incrementa su campo tree en uno.
-void addTree(queryDataADT qd, const char * nbhName){
+int addTree(queryDataADT qd, const char * nbhName){
     if (qd->arrStatus == UNPREPARED){
         qd->arrStatus = PREPARED;
-        listToArray(qd);
+        if( listToArray(qd) == ERROR)
+            return ERROR;
     }
     int index = binSearch( nbhName, qd->nbhArr, qd->arrDim);
 
-    #if DEBUG
-        printf("\nindex: %d\n", index);
-    #endif
-
     if(index >= 0)
         (qd->nbhArr[index].trees)++;
-}
-
-// Ordena el vector siguiendo un criterio determinado de acuerdo a la query que se desee (pasandole el mismo como parametro utilizando un puntero a funcion)
-static nbhList arrayToList(nbhInfo * arr, size_t size, int (*criteria) (nbhInfo *, nbhInfo *)) {
-  int ok = 0; // no se usa en esta funcion
-  nbhList first = NULL;
-  for (size_t i = 0; i < size; i++) {
-    first = addNbhRec(first, arr + i, &ok, criteria);
-  }
-  return first;
+    return SUCCESS;
 }
 
 //Libera la lista de la estructura
@@ -190,26 +245,64 @@ static void freeList(nbhList first){
   }
 }
 
+/* Ordena el vector siguiendo un criterio determinado de acuerdo a la query que
+* se desee (pasandole el mismo como parametro utilizando un puntero a funcion)
+*/
+static nbhList arrayToList(nbhInfo * arr, size_t size, int (*criteria) (nbhInfo *, nbhInfo *)) {
+  int ok;
+  nbhList first = NULL;
+  for (size_t i = 0; i < size; i++) {
+    ok = !SUCCESS;
+    first = addNbhRec(first, arr + i, &ok, criteria);
+    if (ok == ERROR) {
+        freeList(first);
+        return NULL;
+    }
+  }
+  return first;
+}
+
 void toBegin(queryDataADT qd){
   qd->current = qd->firstNbh;
 }
 
-void beginQuery1(queryDataADT qd){
+int beginQuery(queryDataADT qd, int queryNum){
+  static pCompFunc compArr[MAX_QUERY] = {compareQ1, compareQ2, compareQ3};
+  if(queryNum <= 0 || queryNum > MAX_QUERY)
+    return ERROR;
+  freeList(qd->firstNbh);
+  // Crear la lista ordenada de acuerdo al criterio de la query1
+  qd->firstNbh = arrayToList(qd->nbhArr, qd->listDim, compArr[queryNum-1]);
+  if(qd->firstNbh == NULL)
+      return ERROR;
+  // Setear iterador a head
+  toBegin(qd);
+  qd->currQuery = queryNum;
+  return SUCCESS;
+}
+
+int beginQuery1(queryDataADT qd){
   freeList(qd->firstNbh);
   // Crear la lista ordenada de acuerdo al criterio de la query1
   qd->firstNbh = arrayToList(qd->nbhArr, qd->listDim, compareQ1);
+  if(qd->firstNbh == NULL)
+      return ERROR;
   // Setear iterador a head
   toBegin(qd);
   qd->currQuery = 1;
+  return SUCCESS;
 }
 
-void beginQuery2(queryDataADT qd){
+int beginQuery2(queryDataADT qd){
   freeList(qd->firstNbh);
   // Crear la lista ordenada de acuerdo al criterio de la query1
   qd->firstNbh = arrayToList(qd->nbhArr, qd->listDim, compareQ2);
+  if(qd->firstNbh == NULL)
+      return ERROR;
   // Setear iterador a head
   toBegin(qd);
   qd->currQuery = 2;
+  return SUCCESS;
 }
 
 
@@ -242,6 +335,7 @@ char ** answer(queryDataADT qd, size_t * size) {
     case 2:
       // BARRIO;CANT_ARBOLES/HABITANTES
       ans = infoQ2(qd->current->info);
+      *size = 2;
       break;
     case 3:
       // NOMBRE_CIENTIFICO;DIAM_PROMEDIO
