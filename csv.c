042fcd34afe1b116867e0cd0b691092b4ba6f82d
lvittor <lvittor@itba.edu.ar>
2020-07-09 00:53:06 -0300

    Control de memoria y borrado de funciones redundantes

--- csv.c
+++ csv.c
@@ -3,50 +3,40 @@
 #include <string.h>
 #include "csv.h"
 
-FILE * openCSV(const char * path) {
-    FILE * file = fopen(path, "r");
-    if (file == NULL) {
-        fprintf(stderr, "Cannot open %s\n", path);
-        return NULL;
-    }
-    return file;
-}
-
 static char * strDuplicate(const char * src) {
     char * dst = malloc(strlen(src) + 1);   // Guarda espacio para el nuevo string.
-    if (dst != NULL)                        // Si hay memoria,
-      strcpy(dst, src);                     // hace la copia
+    if (dst == NULL)                        // Si no hay memoria,
+        perror("Error en malloc()");        // error.
+    else                                    // Si no,
+        strcpy(dst, src);                   // hace la copia
     return dst;                             // Retorna el nuevo string
 }
 
 char ** getColumns(const char * line, const size_t * desiredColumns, size_t quantity) {
     char ** ans = calloc(quantity, sizeof(ans[0]));
+    if(ans == NULL){
+        perror("Error en calloc()");
+        return NULL;
+    }
 
     char * tmp = strDuplicate(line);
-    char * token = strtok(tmp, DELIMITER);
+    if(tmp == NULL)
+        return NULL;
 
-    if (token == NULL) {
-        fprintf(stderr, "format error in csv file\n");
-        exit(1); // preguntar si hay que hacer exit() o devolver NULL
-    }
+    char * token = strtok(tmp, DELIMITER); // no verificamos token, asumimos formato correcto.
 
     size_t currField = 0, currCol = 0;
     do {
-        if (desiredColumns[currField] == currCol)
-          ans[currField++] = strDuplicate(token);
-
+        if (desiredColumns[currField] == currCol){
+          char * aux = strDuplicate(token);
+          if(aux == NULL)
+              return NULL;
+          ans[currField++] = aux;
+        }
         currCol++;
-    } while(currField < quantity && (token = strtok(NULL, DELIMITER)) != NULL);
+    } while(currField < quantity && (token = strtok(NULL, DELIMITER)) != NULL);  // no verificamos token, asumimos formato correcto.
 
     free(tmp);
 
     return ans;
-}
-
-char * nextLine(FILE * file, char * line, size_t maxLen) {
-    return fgets(line, maxLen, file);
-}
-
-void closeCSV(FILE * file) {
-    fclose(file);
 }
