33d2ddc5c62ffc558c0ac0abfd142e1eda1b41e2
mbartellini <mbartellini@itba.edu.ar>
2020-07-14 19:24:27 -0300

    Creación de función para exit limpio en main.c

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #define MAX_OUTPUT_NAME 15 // Tamaño maximo del nombre del archivo de salida
 #define MAX_BUFF 1024    // Tamaño maximo del buffer de lectura
 
+#define RETURN_ERROR    1
+
 #define FIELDS_TREE 3 // Cantidad de campos relevantes a leer de arboles.csv
 
 /* Número de columna de Comuna, Nombre cientifico, Diametro en arboles.csv
@@ -40,10 +42,13 @@ void printArrayOfStrings(char ** strArr, size_t size);
 /* Función: Permite abrir liberar la memoria del vector de strings generado por getColumns */
 void freeVec(char ** vec, size_t size);
 
+/* Función: Libera memoria alocada y cierra archivos cuando se quiere salir del programa */
+int safeExit(queryDataADT qd, FILE * file);
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Debe pasar exactamente 2 argumentos.\n");
-        return 1;
+        return RETURN_ERROR;
     }
 
     char * treeCSVPath = argv[1];
@@ -52,13 +57,15 @@ int main(int argc, char *argv[]) {
     char line[MAX_BUFF];
     FILE * file;
     queryDataADT qd = newQueryData();
+    if (qd == NULL)
+        return RETURN_ERROR;
 
     /* --- Lectura de datos de barrios.csv ---*/
 
     if((file = fopen(nbhCSVPath, "r")) == NULL){
         perror("Error leyendo el archivo de barrios");
         freeQueryData(qd);
-        return 1;
+        return RETURN_ERROR;
     }
 
     fgets(line, MAX_BUFF, file); // remueve el header del .csv
@@ -68,11 +75,8 @@ int main(int argc, char *argv[]) {
             printf("Se leyo el barrio:\n");
             printArrayOfStrings(rowData, FIELDS_NBH);
         #endif
-        if (addNbh(qd, rowData[NBH_NAME], atoi(rowData[NBH_POP])) == ERROR) {
-            fclose(file);
-            freeQueryData(qd);
-            return 1;
-        }
+        if (addNbh(qd, rowData[NBH_NAME], atoi(rowData[NBH_POP])) == ERROR)
+            return safeExit(qd, file);
         free(rowData);
     }
 
@@ -83,7 +87,7 @@ int main(int argc, char *argv[]) {
     if((file = fopen(treeCSVPath, "r")) == NULL){
         perror("Error leyendo el archivo de arboles");
         freeQueryData(qd);
-        return 1;
+        return RETURN_ERROR;
     }
 
     fgets(line, MAX_BUFF, file); // remueve el header del .csv
@@ -93,11 +97,8 @@ int main(int argc, char *argv[]) {
             printf("Se leyo el arbol:\n");
             printArrayOfStrings(rowData, FIELDS_TREE);
         #endif
-        if (addTree(qd, rowData[TREE_NBH], rowData[TREE_SCI_NAME], atof(rowData[TREE_DIAM])) == ERROR) {
-            fclose(file);
-            freeQueryData(qd);
-            return 1;
-        }
+        if (addTree(qd, rowData[TREE_NBH], rowData[TREE_SCI_NAME], atof(rowData[TREE_DIAM])) == ERROR)
+            return safeExit(qd, file);
         free(rowData);
     }
 
@@ -110,24 +111,23 @@ int main(int argc, char *argv[]) {
 
         char outputName[MAX_OUTPUT_NAME];
         snprintf(outputName, MAX_OUTPUT_NAME, "query%d.csv", i);
-        file = fopen(outputName, "w");
 
-        if(beginQuery(qd, i) == ERROR){
-            fclose(file);
-            freeQueryData(qd);
-            return 1;
+        if ( (file = fopen(outputName, "w")) == NULL) {
+            perror("Error creando el archivo de salida");
+            return RETURN_ERROR;
         }
+
+        if(beginQuery(qd, i) == ERROR)
+            return safeExit(qd, file);
 
         fprintf(file, "%s\n", headers[i-1]);
 
         size_t size;
         while(hasNext(qd)) {
             char ** ans = answer(qd, &size);
-            if (ans == NULL) {
-                fclose(file);
-                freeQueryData(qd);
-                return 1;
-            }
+            if (ans == NULL)
+                return safeExit(qd, file);
+
             #if DEBUG
                 printArrayOfStrings(ans, size);
             #endif
@@ -152,4 +152,12 @@ void freeVec(char ** vec, size_t size){
   for(int i = 0; i < size; i++)
       free(vec[i]);
   free(vec);
+}
+
+int safeExit(queryDataADT qd, FILE * file) {
+    if (qd != NULL)
+        freeQueryData(qd);
+    if (file != NULL)
+        fclose(file);
+    return RETURN_ERROR;
 }
